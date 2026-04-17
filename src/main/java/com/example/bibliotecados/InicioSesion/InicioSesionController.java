2e513f389c50516176e58ccb9293210fa78aa8d9
AxelLJ57 <20253rd038@utez.edu.mx>
2026-04-17 15:27:22 -0600

    Se creo el menu con los botones de agregar y buscar libro y se enlazo la ventana de inicio de sesion con el menu

--- src/main/java/com/example/bibliotecados/InicioSesion/InicioSesionController.java
+++ src/main/java/com/example/bibliotecados/InicioSesion/InicioSesionController.java
@@ -2,9 +2,16 @@ package com.example.bibliotecados.InicioSesion;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class InicioSesionController {
     @FXML
     private TextField txtUser;
@@ -28,6 +35,18 @@ public class InicioSesionController {
         boolean valido = userModel.validarUsuario(usuario, password);
         if (valido) {
             lblMessage.setText("Bienvenido "+usuario);
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Menu.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setTitle("Segunda Ventana");
+                stage.setScene(new Scene(root));
+                stage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }else {
             lblMessage.setText("Datos incorrectos");
         }
